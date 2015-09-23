b12ee2c21fc08c80d67a0926b1084dd22fc6cff3
Jakub Hrozek <jakub.hrozek@posteo.se>
2015-09-23 16:23:58 +0200

    pwrap: Wrap pam_chauthtok

--- tests/pam_example.c
+++ tests/pam_example.c
@@ -30,6 +30,17 @@
 	}							\
 } while(0);
 
+
+#define wipe_authtok(tok) do {		\
+	if (tok != NULL) {		\
+		char *__tk = tok;	\
+		while(*__tk != '\0') {	\
+			*__tk = '\0';	\
+		}			\
+		tok = NULL;		\
+	}				\
+} while(0);
+
 struct pam_lib_items {
 	const char *username;
 	const char *service;
@@ -46,7 +57,6 @@ struct pam_example_ctx {
 	struct pam_example_mod_items pmi;
 };
 
-
 static int pam_example_mod_items_get(const char *username,
 				     struct pam_example_mod_items *pmi)
 {
@@ -60,7 +70,7 @@ static int pam_example_mod_items_get(const char *username,
 
 	db = getenv("PWRAP_PASSDB");
 	if (db == NULL) {
-		rv = EIO;
+		rv = PAM_AUTHINFO_UNAVAIL;
 		goto fail;
 	}
 
@@ -112,6 +122,7 @@ static int pam_example_mod_items_get(const char *username,
 		}
 	}
 
+	fclose(fp);
 	return 0;
 
 fail:
@@ -123,6 +134,107 @@ fail:
 	return rv;
 }
 
+static int pam_example_lib_items_put(struct pam_lib_items *pli)
+{
+	int rv;
+	const char *db;
+	FILE *fp = NULL;
+	FILE *fp_tmp = NULL;
+	char buf[BUFSIZ];
+	char template[PATH_MAX] = { '\0' };
+	char *file_user = NULL;
+	char *file_password = NULL;
+	char *file_svc = NULL;
+
+	db = getenv("PWRAP_PASSDB");
+	if (db == NULL) {
+		rv = PAM_AUTHINFO_UNAVAIL;
+		goto done;
+	}
+
+	rv = snprintf(template, sizeof(template),
+		      "%s.XXXXXX", db);
+	if (rv <= 0) {
+		rv = PAM_BUF_ERR;
+		goto done;
+	}
+
+	/* We don't support concurrent runs.. */
+	rv = mkstemp(template);
+	if (rv <= 0) {
+		rv = PAM_BUF_ERR;
+		goto done;
+	}
+
+	fp = fopen(db, "r");
+	fp_tmp = fopen(template, "w");
+	if (fp == NULL || fp_tmp == NULL) {
+		rv = errno;
+		goto done;
+	}
+
+	while (fgets(buf, sizeof(buf), fp) != NULL) {
+		char *q;
+
+		file_user = buf;
+		file_password = NULL;
+
+		/* Skip comments */
+		if (file_user[0] == '#') {
+			continue;
+		}
+
+		/* Find the user, his password and allowed service */
+		NEXT_KEY(file_user, file_password);
+		NEXT_KEY(file_password, file_svc);
+
+		q = file_svc;
+		while(q[0] != '\n' && q[0] != '\0') {
+			q++;
+		}
+		q[0] = '\0';
+
+		if (file_password == NULL) {
+			continue;
+		}
+
+		if (strcmp(file_user, pli->username) == 0) {
+			if (pli->password) {
+				file_password = pli->password;
+			}
+		}
+
+		rv = fprintf(fp_tmp, "%s\t%s\t%s\n",
+			     file_user, file_password, file_svc);
+		if (rv < 0) {
+			rv = PAM_CRED_ERR;
+			goto done;
+		}
+	}
+
+	rv = PAM_SUCCESS;
+done:
+	if (fp != NULL) {
+		fclose(fp);
+	}
+	if (fp_tmp != NULL) {
+		fflush(fp_tmp);
+		fclose(fp_tmp);
+	}
+
+	if (rv == PAM_SUCCESS) {
+		rv = rename(template, db);
+		if (rv == -1) {
+			rv = PAM_SYSTEM_ERR;
+		}
+	}
+
+	if (template[0] != '\0') {
+		unlink(template);
+	};
+	return rv;
+}
+
 static void pam_example_mod_items_free(struct pam_example_mod_items *pmi)
 {
 	if (pmi == NULL) {
@@ -131,6 +243,70 @@ static void pam_example_mod_items_free(struct pam_example_mod_items *pmi)
 
 	free(pmi->password);
 	free(pmi->service);
+}
+
+static int pam_example_read_password(pam_handle_t *pamh,
+				     int authtok_item,
+				     const char *prompt1,
+				     const char *prompt2,
+				     const void **_out_tok)
+{
+	int rv = PAM_AUTHTOK_RECOVERY_ERR;
+	char *authtok1 = NULL;
+	char *authtok2 = NULL;
+	const void *item;
+
+	rv = pam_prompt(pamh, PAM_PROMPT_ECHO_OFF,
+			&authtok1, "%s", prompt1);
+	if (authtok1 == NULL) {
+		goto done;
+	}
+
+	if (rv == PAM_SUCCESS && prompt2 != NULL) {
+		rv = pam_prompt(pamh, PAM_PROMPT_ECHO_OFF,
+				&authtok2, "%s", prompt2);
+		if (rv != PAM_SUCCESS) {
+			goto done;
+		}
+
+		if (authtok2 == NULL) {
+			rv = PAM_AUTHTOK_RECOVERY_ERR;
+			goto done;
+		}
+
+		if (strcmp(authtok1, authtok2) != 0) {
+			pam_prompt(pamh, PAM_ERROR_MSG, NULL,
+				   "%s", "Passwords do not match");
+			rv = PAM_AUTHTOK_RECOVERY_ERR;
+			goto done;
+		}
+		wipe_authtok(authtok2);
+	}
+
+	if (rv != PAM_SUCCESS) {
+		goto done;
+	}
+
+	rv = pam_set_item(pamh, authtok_item, authtok1);
+	wipe_authtok(authtok1);
+	if (rv != PAM_SUCCESS) {
+		goto done;
+	}
+
+	rv = pam_get_item(pamh, authtok_item, &item);
+	if (_out_tok) {
+		*_out_tok = item;
+	}
+	item = NULL;
+	if (rv != PAM_SUCCESS) {
+		goto done;
+	}
+
+	rv = PAM_SUCCESS;
+done:
+	wipe_authtok(authtok1);
+	wipe_authtok(authtok2);
+	return rv;
 }
 
 static int pam_lib_items_get(pam_handle_t *pamh,
@@ -152,16 +328,6 @@ static int pam_lib_items_get(pam_handle_t *pamh,
 		return rv;
 	}
 
-	rv = pam_prompt(pamh, PAM_PROMPT_ECHO_OFF,
-			&pli->password, "%s", "Password");
-	if (rv != PAM_SUCCESS) {
-		return rv;
-	}
-
-	if (pli->password == NULL) {
-		return PAM_AUTHINFO_UNAVAIL;
-	}
-
 	return PAM_SUCCESS;
 }
 
@@ -171,12 +337,12 @@ static int pam_example_get(pam_handle_t *pamh, struct pam_example_ctx *pe_ctx)
 
     rv = pam_lib_items_get(pamh, &pe_ctx->pli);
     if (rv != PAM_SUCCESS) {
-	    return rv;
+		return rv;
     }
 
     rv = pam_example_mod_items_get(pe_ctx->pli.username, &pe_ctx->pmi);
     if (rv != PAM_SUCCESS) {
-	    return rv;
+		return PAM_AUTHINFO_UNAVAIL;
     }
 
     return PAM_SUCCESS;
@@ -185,6 +351,31 @@ static int pam_example_get(pam_handle_t *pamh, struct pam_example_ctx *pe_ctx)
 static void pam_example_free(struct pam_example_ctx *pe_ctx)
 {
 	pam_example_mod_items_free(&pe_ctx->pmi);
+}
+
+static int _pam_example_auth(struct pam_example_ctx *pctx)
+{
+	int rv = PAM_AUTH_ERR;
+
+	if (pctx->pli.password != NULL &&
+	    pctx->pmi.password != NULL &&
+	    strcmp(pctx->pli.password, pctx->pmi.password) == 0) {
+		rv = PAM_SUCCESS;
+	}
+
+	return rv;
+}
+
+static int pam_example_auth(struct pam_example_ctx *pctx)
+{
+	int rv = PAM_AUTH_ERR;
+
+	rv = _pam_example_auth(pctx);
+
+	wipe_authtok(pctx->pli.password);
+	wipe_authtok(pctx->pmi.password);
+
+	return rv;
 }
 
 PAM_EXTERN int
@@ -205,12 +396,13 @@ pam_sm_authenticate(pam_handle_t *pamh, int flags,
 		goto done;
 	}
 
-	if (strcmp(pctx.pli.password, pctx.pmi.password) == 0) {
-		rv = PAM_SUCCESS;
-		goto done;
+	rv = pam_example_read_password(pamh, PAM_AUTHTOK, "Password: ", NULL,
+				       (const void **) &pctx.pli.password);
+	if (rv != PAM_SUCCESS) {
+		return PAM_AUTHINFO_UNAVAIL;
 	}
 
-	rv = PAM_AUTH_ERR;
+	rv = pam_example_auth(&pctx);
 done:
 	pam_example_free(&pctx);
 	return rv;
@@ -246,7 +438,9 @@ pam_sm_acct_mgmt(pam_handle_t *pamh, int flags,
 		goto done;
 	}
 
-	if (strcmp(pctx.pli.service, pctx.pmi.service) == 0) {
+	if (pctx.pli.service != NULL &&
+	    pctx.pmi.service != NULL &&
+	    strcmp(pctx.pli.service, pctx.pmi.service) == 0) {
 		rv = PAM_SUCCESS;
 		goto done;
 	}
@@ -328,10 +522,56 @@ PAM_EXTERN int
 pam_sm_chauthtok(pam_handle_t *pamh, int flags,
 		 int argc, const char *argv[])
 {
-	(void) pamh;  /* unused */
+	struct pam_example_ctx pctx;
+	const char *old_pass;
+	int rv;
+
 	(void) flags; /* unused */
 	(void) argc;  /* unused */
 	(void) argv;  /* unused */
 
-	return PAM_SERVICE_ERR;
+	memset(&pctx, 0, sizeof(struct pam_example_ctx));
+
+	rv = pam_example_get(pamh, &pctx);
+	if (rv != PAM_SUCCESS) {
+		goto done;
+	}
+
+	if (flags & PAM_PRELIM_CHECK) {
+		rv = pam_example_read_password(
+					pamh, PAM_OLDAUTHTOK,
+					"Old password: ", NULL,
+					(const void **) &pctx.pli.password);
+		if (rv != PAM_SUCCESS) {
+			rv = PAM_AUTHINFO_UNAVAIL;
+			goto done;
+		}
+
+		rv = pam_example_auth(&pctx);
+	} else if (flags & PAM_UPDATE_AUTHTOK) {
+		rv = pam_get_item(pamh,
+				  PAM_OLDAUTHTOK,
+				  (const void **) &old_pass);
+		if (rv != PAM_SUCCESS || old_pass == NULL) {
+			rv = PAM_AUTHINFO_UNAVAIL;
+			goto done;
+		}
+
+		rv = pam_example_read_password(pamh, PAM_AUTHTOK,
+					"New Password :",
+					"Verify New Password :",
+					(const void **) &pctx.pli.password);
+		if (rv != PAM_SUCCESS) {
+			rv = PAM_AUTHINFO_UNAVAIL;
+			goto done;
+		}
+
+		rv = pam_example_lib_items_put(&pctx.pli);
+	} else {
+		rv = PAM_SYSTEM_ERR;
+	}
+
+done:
+	pam_example_free(&pctx);
+	return rv;
 }
